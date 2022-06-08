3848ba87fe3efacc3f5bdaab15fbf55deb797bcc
muzzafar <muzfr267@gmail.com>
2022-06-08 08:39:06 +0000

    changes

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var express = require('express')
 var app = express()
 //Define request response in root URL (/) and response with text Hello World!
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World! Deploying this into azure cloud. for DEMO only')
 })
 //Launch listening server on port 8080 and consoles the log.
-app.listen(8080, function () {
+app.listen(80, function () {
   console.log('app listening on port 8080!')
 })
